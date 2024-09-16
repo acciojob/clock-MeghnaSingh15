4b445972ca92708b0d303029f77272a099744d73
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-16 20:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let t = document.getElementById("timer");
+
+let currDate = new Date();
+
+setInterval(function() {
+	currDate = new Date();
+	t.innerHTML = currDate.toLocaleString();
+	
+},1000)
